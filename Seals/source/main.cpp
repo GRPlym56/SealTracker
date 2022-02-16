2129ac225e83328fc9feea167b387e004b509b4a
GRPlym56 <64791542+GRPlym56@users.noreply.github.com>
2022-02-16 15:51:02 +0000

    Test program

--- Seals/source/main.cpp
+++ Seals/source/main.cpp
@@ -1,10 +1,31 @@
 #include "mbed.h"
 
 // main() runs in its own thread in the OS
+
+EventQueue PrintQueue;
+Thread PrintThread;
+Thread TestThread;
+DigitalOut LED(PB_3);
+
+void flash(void);
+void printer(void);
+
 int main()
 {
-    while (true) {
-        ThisThread::sleep_for(500s);
+    PrintThread.start(printer);
+    TestThread.start(flash);
+
+}
+
+void flash()
+{
+    while(1){
+        LED = !LED;
+        ThisThread::sleep_for(1s);
     }
 }
 
+void printer()
+{
+    PrintQueue.dispatch_forever();
+} 
