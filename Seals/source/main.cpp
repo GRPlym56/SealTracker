219f151ffe53929b910f384e8ddf6a98541db688
GRPlym56 <64791542+GRPlym56@users.noreply.github.com>
2022-03-07 14:10:58 +0000

    Removed unnecessary code

--- Seals/source/main.cpp
+++ Seals/source/main.cpp
@@ -1,35 +1,40 @@
 #include "mbed.h"
 #include "stm32l432xx.h"
 #include "stm32l4xx_hal.h"
+#include "LowPowerTicker.h"
 
 // main() runs in its own thread in the OS
 
-RTC_HandleTypeDef hrtc;
+//RTC_HandleTypeDef hrtc;
 
 //EventQueue PrintQueue;
 //Thread PrintThread;
 //Thread TestThread;
 DigitalOut LED(PB_3);
 
+LowPowerTicker Tick;
+void flashinterrupt(void);
 
-
-
+/*
 void Error_Handler(void);
 
 void SystemClock_Config(void);
 static void MX_RTC_Init(void);
+*/
 
 int main()
 {
 
-    HAL_Init();
-    SystemClock_Config();
-    MX_RTC_Init();
+    //HAL_Init();
+    //SystemClock_Config();
+    //MX_RTC_Init();
+    
+    Tick.attach(flashinterrupt, 2s);
     
     
-    
-    
-    
+    //hal_deepsleep();
+
+
     while(1)
     {
         //HAL_PWR_EnterSLEEPMode(PWR_MAINREGULATOR_ON, PWR_SLEEPENTRY_WFI);
@@ -38,9 +43,6 @@ int main()
         //ThisThread::sleep_for(500ms);
     }
     
-    
-    
-
 }
 
 void flashinterrupt()
@@ -49,91 +51,5 @@ void flashinterrupt()
 }
 
 
-void SystemClock_Config(void) //got stm32cube to make this for me and then i nicked it
-{
-  RCC_OscInitTypeDef RCC_OscInitStruct = {0};
-  RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
-  /** Configure the main internal regulator output voltage
-  */
-  /*
-  if (HAL_PWREx_ControlVoltageScaling(PWR_REGULATOR_VOLTAGE_SCALE1) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  */
-  /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
-  RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_LSI|RCC_OSCILLATORTYPE_MSI;
-  RCC_OscInitStruct.LSIState = RCC_LSI_ON;
-  RCC_OscInitStruct.MSIState = RCC_MSI_ON;
-  RCC_OscInitStruct.MSICalibrationValue = 0;
-  RCC_OscInitStruct.MSIClockRange = RCC_MSIRANGE_5;
-  RCC_OscInitStruct.PLL.PLLState = RCC_PLL_NONE;
-  /*
-  if (HAL_RCC_OscConfig(&RCC_OscInitStruct) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  */
-  /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
-  RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_MSI;
-  RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
-  RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
-  RCC_ClkInitStruct.APB2CLKDivider = RCC_HCLK_DIV1;
-
-  /*
-  if (HAL_RCC_ClockConfig(&RCC_ClkInitStruct, FLASH_LATENCY_0) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  */
-}
-
-static void MX_RTC_Init(void)
-{
-
-  /* USER CODE BEGIN RTC_Init 0 */
-
-  /* USER CODE END RTC_Init 0 */
-
-  /* USER CODE BEGIN RTC_Init 1 */
-
-  /* USER CODE END RTC_Init 1 */
-  /** Initialize RTC Only
-  */
-  hrtc.Instance = RTC;
-  hrtc.Init.HourFormat = RTC_HOURFORMAT_24;
-  hrtc.Init.AsynchPrediv = 127;
-  hrtc.Init.SynchPrediv = 255;
-  hrtc.Init.OutPut = RTC_OUTPUT_DISABLE;
-  hrtc.Init.OutPutRemap = RTC_OUTPUT_REMAP_NONE;
-  hrtc.Init.OutPutPolarity = RTC_OUTPUT_POLARITY_HIGH;
-  hrtc.Init.OutPutType = RTC_OUTPUT_TYPE_OPENDRAIN;
-  /*
-  if (HAL_RTC_Init(&hrtc) != HAL_OK)
-  {
-    Error_Handler();
-  }
-  */
-  /* USER CODE BEGIN RTC_Init 2 */
-
-  /* USER CODE END RTC_Init 2 */
-
-}
-
-void Error_Handler(void) //this does nothing, STM32cube thought it was a good idea ¯\_(ツ)_/¯
-{
-  /* USER CODE BEGIN Error_Handler_Debug */
-  /* User can add his own implementation to report the HAL error return state */
-  __disable_irq();
-  while (1)
-  {
-  }
-  /* USER CODE END Error_Handler_Debug */
-}
 
